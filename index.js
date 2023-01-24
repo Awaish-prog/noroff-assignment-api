9aa3791df891ff6e717b8d96b3271987b587abbb
unknown <awaishk640@gmail.com>
2023-01-24 09:06:31 +0530

    cors

--- index.js
+++ index.js
@@ -15,11 +15,7 @@ const HTTP_METHOD_GET = 'get'
 
 server.use(middlewares)
 server.use(cors({origin:"http://localhost:3000"}));
-server.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", req.headers.origin);
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+
 server.use((request, response, next) => {
 
     if (request.method.toLowerCase() !== HTTP_METHOD_GET) {
@@ -43,7 +39,11 @@ server.use((request, response, next) => {
 })
 
 server.use(router)
-
+server.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 server.listen(PORT, () => {
     console.log('JSON Server is running in port: ' + PORT)
 })
